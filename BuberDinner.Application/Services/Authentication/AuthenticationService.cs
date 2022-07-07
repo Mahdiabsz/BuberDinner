8bf6418dd8382908e3cc7ec175626ddf8fc43ef9
mahdiabsz <mahdiabbaszadeh1376@gmail.com>
2022-07-07 12:34:32 +0430

    added jwt to project

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,24 +9,36 @@ namespace BuberDinner.Application.Services.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private IJwtTokenGenerator _jwtTokenGenerator;
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
         public AuthenticationResult Login(string email, string password)
         {
+            var userId = Guid.NewGuid();
+            var token = _jwtTokenGenerator.GenerateToken(userId, "firstName", "lastName");
+
             return new AuthenticationResult(
-                Guid.NewGuid(),
+                userId,
                 "firstname",
                 "lastname",
                 email,
-                "token");
+                token);
         }
 
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
+            var userId = Guid.NewGuid();
+            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
             return new AuthenticationResult(
-                Guid.NewGuid(),
+                userId,
                 firstName,
                 lastName,
                 email,
-                "token");
+                token);
         }
     }
 }
